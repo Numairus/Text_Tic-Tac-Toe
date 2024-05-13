929e4cb161726625c5a8296b5092462deb679029
Numairus <stridere@amazon.com>
2024-05-13 14:01:51 -0500

    Add checks for winning moves: horizontal, vertical, diagonal

--- game_master.py
+++ game_master.py
@@ -4,60 +4,55 @@ class GameMaster:
         self.player1_character = p1_char
         self.player2_character = p2_char
         self.player_turn = 0
+        self.diagonals = [2, 6, 4, 0, 8]
 
-    def win_check(self, position):
-        # Convert player position choice to board index
-        i = position - 1
-        # Check win conditions based on the last played position
-        match position:
-            case "1": # Top left corner
-                # Check 2/3
-                # Check 4/7
-                # Check 5/9
-                if (self.board[0] == self.board[1] == self.board[2]  # Top row
-                        or self.board[0] == self.board[3] == self.board[6]  # Left column
-                        or self.board[0] == self.board[4] == self.board[8]):  # Diagonal, top left to bottom right
-                    return True
-            case "2":
-                # Check 1/3
-                # Check 5/8
-                if (self.board[0] == self.board[1] == self.board[2] # Top row
-                        or self.board[1] == self.board[4] == self.board[7]): # Middle column
-                    return True
-            case "3":
-                # Check 1/2
-                # Check 6/9
-                # Check 5/7
-                pass
-            case "4":
-                # Check 1/7
-                # Check 5/6
-                pass
-            case "5":
-                # Check 4/6
-                # Check 2/8
-                # Check 1/9
-                # Check 3/7
-                pass
-            case "6":
-                # Check 4/5
-                # Check 3/9
-                pass
-            case "7":
-                # Check 1/4
-                # Check 3/5
-                # Check 8/9
-                pass
-            case "8":
-                # Check 7/9
-                # Check 2/5
-                pass
-            case "9":
-                # Check 1/5
-                # Check 7/8
-                # Check 3/6
-                pass
-        pass
+    def win_check(self, play):
+        # If a diagonal is chosen then check diagonals too, otherwise just check horizontal/vertical
+        diagonal = False
+        if play in self.diagonals:
+            horizontal = self.check_horizontal(play)
+            vertical = self.check_vertical(play)
+            diagonal = self.check_diagonal(play)
+        else:
+            horizontal = self.check_horizontal(play)
+            vertical = self.check_vertical(play)
+
+        print(f"horizontal check is {horizontal}")
+        print(f"vertical check is {vertical}")
+        print(f"diagonal check is {diagonal}")
+        return True in [horizontal, vertical, diagonal]
+
+
+    def check_horizontal(self, p):
+        # Check values on the same row to see if they're all the same
+        values = [p]  # List of values to check, adding chosen move
+        r = p // 3 * 3  # Row offset
+
+        # Calculate the other 2 positions to check on the same row as the player's choice
+        for i in range(1, 3):
+            values.append((p + i) % 3 + r)
+
+        return self.board[values[0]] == self.board[values[1]] == self.board[values[2]]
+
+    def check_vertical(self, p):
+        # Check values in the same column to see if they're all the same
+        values = [p]  # List of values to check, adding chosen move
+
+        # Calculate the other 2 positions to check in the same column as the player's choice
+        for i in range(1, 3):
+            values.append((p + 3 * i) % 9)
+
+        return self.board[values[0]] == self.board[values[1]] == self.board[values[2]]
+
+    def check_diagonal(self, p):
+        # Check values in the diagonals for 3 in a row
+        # There are only 2 diagonals, so it's just easier to hard code those
+
+        if (self.board[self.diagonals[0]] == self.board[self.diagonals[1]] == self.board[self.diagonals[2]]
+                or self.board[self.diagonals[2]] == self.board[self.diagonals[3]] == self.board[self.diagonals[4]]):
+            return True
+        else:
+            return False
 
     def ai_play(self, character, move):
         self.board[move - 1] = character
@@ -73,10 +68,16 @@ class GameMaster:
     def player_turn(self):
         player_move = input(f"Player {self.player_turn() + 1}, what's your move? ")
 
-    def update_board(self, player, position):
+    def update_board(self, position):
         i = int(position) - 1
+        player = self.player_turn
 
         if player == 1:
             self.board[i] = self.player1_character
         elif player == 2:
             self.board[i] = self.player2_character
+
+        self.win_check(i)
+
+    def next_turn(self):
+        self.player_turn = self.player_turn % 2 + 1
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from game_master import GameMaster
 
 if __name__ == '__main__':
-    # Initial setup: Setting player characters, turn, and drawing game board
+    # Initial setup: Setting player characters, initialize game master
     player1_character = "x"
     player2_character = "o"
     character_choice = input("Player 1, would you like to be 'x' or 'o'? ")
@@ -13,18 +13,17 @@ if __name__ == '__main__':
     print(f"Player 1 is {player1_character}")
     print(f"Player 2 is {player2_character} ")
 
-    player_turn = 1
-    game_master.draw_board()
-
     # Game loop
     while True:
+        # Increment/alternate player turn
+        # Draw board
         # Get player move input
         # Update board with given player move input
-        # Draw updated board
-        # Increment/alternate player turn
+
+        game_master.next_turn()
         print(f"Turn {game_master.player_turn}")
-        player_move = input(f"Player {player_turn}, what's your move? ")
-        game_master.update_board(player_turn, player_move)
         game_master.draw_board()
-        player_turn = player_turn % 2 + 1
+        player_move = input(f"Player {game_master.player_turn}, what's your move? ")
+        game_master.update_board(player_move)
+
 
